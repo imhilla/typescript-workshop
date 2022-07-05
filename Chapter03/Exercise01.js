79087f1cab5851565ea23b30f4e7160c6dfabe0b
imhilla <hillaryodhiambo282@gmail.com>
2022-07-05 19:46:34 +0300

    add chapter 3 content

---
+++ Chapter03/Exercise01.js
@@ -0,0 +1,7 @@
+var values = [8, 42, 99, 161];
+var total = 0;
+for (var i = 0; i < values.length; ++i) {
+    total += values[i];
+}
+var average = total / values.length;
+console.log(average);
---
+++ Chapter03/Exercise01.ts
@@ -0,0 +1,8 @@
+const values = [8, 42, 99, 161];
+let total = 0;
+for (let i = 0; i < values.length; ++i) {
+  total += values[i];
+}
+const average = total / values.length;
+console.log(average);
+// npx ts-node filename.ts
