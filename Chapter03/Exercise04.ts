f5dc8899334f48231851ec0d93e17a4ca8bb8c70
imhilla <hillaryodhiambo282@gmail.com>
2022-07-08 05:49:04 +0300

    update app

--- Chapter03/Exercise04.ts
+++ Chapter03/Exercise04.ts
@@ -3,10 +3,21 @@ export const account = {
   paid: 0,
   status: "OPEN",
   payAccount: function (amount: number): string {
-    return "Unimplemented";
+    if (amount > this.due - this.paid) {
+      return `$${amount} is more than the outstanding balance of $${
+        this.due - this.paid
+      }`;
+    }
+    this.paid += amount;
+    if (this.paid == this.due) {
+      this.status = "CLOSED";
+    }
+    return this.printStatus();
   },
   printStatus: function (): string {
-    return "Unimplemented";
+    return `$${this.paid} has been paid and $${
+      this.due - this.paid
+    } is outstanding. This account is ${this.status}`;
   },
 };
 
