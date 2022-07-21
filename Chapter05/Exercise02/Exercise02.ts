02fa8bc7adc1a3206e5166782277c967e26b19cb
imhilla <hillaryodhiambo282@gmail.com>
2022-07-21 04:59:31 +0300

    update blog post

--- Chapter05/Exercise02/Exercise02.ts
+++ Chapter05/Exercise02/Exercise02.ts
@@ -12,3 +12,28 @@ interface IBlogPost {
   allPost: BlogPost[];
   addToPost: AddToPost;
 }
+
+class blogPostClass implements IBlogPost {
+  allPost: BlogPost[] = [];
+  addToPost(post: BlogPost): BlogPost[] {
+    this.allPost = [...this.allPost, post];
+    return this.allPost;
+  }
+}
+
+const blog = new blogPostClass();
+let post1: BlogPost = {
+  post: "Goodbye, 2020",
+  timeStamp: (12345678).toString(),
+  user: "Rayon",
+};
+let post2: BlogPost = {
+  post: "Welcome, 2021",
+  timeStamp: (12345678).toString(),
+  user: "Mark",
+};
+let post3: BlogPost = {
+  post: "What happened to 1999?",
+  timeStamp: (12345678).toString(),
+  user: "Will",
+};
