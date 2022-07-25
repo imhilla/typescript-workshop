cf1e56e63b5683cb721edd55232ca91234a61a0d
imhilla <hillaryodhiambo282@gmail.com>
2022-07-25 04:23:10 +0300

    update app

--- Chapter06/Exercise02.ts
+++ Chapter06/Exercise02.ts
@@ -15,5 +15,14 @@ export type Order = {
   amount: number;
   discount?: number;
 };
+
 // intersection
 type Cart = Product & Order;
+const cart: Cart = {
+  name: "Mango",
+  price: 400,
+  orderId: "sfdfdvdvd",
+  amount: 3,
+  description: "This is a description",
+};
+console.log(cart);
