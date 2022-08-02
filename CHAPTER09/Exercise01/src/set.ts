af8ab884adf188991bf491107370c8e4169ab17a
imhilla <hillaryodhiambo282@gmail.com>
2022-08-02 05:28:17 +0300

    add all set methods

--- CHAPTER09/Exercise01/src/set.ts
+++ CHAPTER09/Exercise01/src/set.ts
@@ -3,4 +3,21 @@ class Set<T> {
   constructor(initialItems: T[] = []) {
     this.items = initialItems;
   }
+  get size(): number {
+    return this.items.length;
+  }
+  has(item: T): boolean {
+    return this.items.includes(item);
+  }
+  add(item: T): void {
+    if (!this.has(item)) {
+      this.items.push(item);
+    }
+  }
+  remove(item: T): void {
+    const itemIndex = this.items.indexOf(item);
+    if (itemIndex >= 0) {
+      this.items.splice(itemIndex, 1);
+    }
+  }
 }
