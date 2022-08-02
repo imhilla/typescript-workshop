4d0b2e3f074aefe322bb08c6be3cfaa7e48cf080
imhilla <hillaryodhiambo282@gmail.com>
2022-08-02 06:00:39 +0300

    pass dynamic generic

---
+++ CHAPTER09/generic.ts
@@ -0,0 +1,28 @@
+interface FetchResponse<T> {
+  status: number;
+  headers: Headers;
+  data: T;
+}
+
+async function fetchJson<T>(url: string): Promise<FetchResponse<T>> {
+  const response = await fetch(url);
+  return {
+    headers: response.headers,
+    status: response.status,
+    data: await response.json(),
+  };
+}
+
+(async () => {
+  interface Todo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+  const { data } = await fetchJson<Todo>(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
+
+  console.log(data);
+})();
