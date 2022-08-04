ee1142e7f218402119a01ac8d0795512e1a38a89
imhilla <hillaryodhiambo282@gmail.com>
2022-08-04 05:06:29 +0300

    update app

--- CHAPTER09/Exercise02/src/memoize.ts
+++ CHAPTER09/Exercise02/src/memoize.ts
@@ -1,1 +1,23 @@
-function memoize(fn: Function, keyGetter?: (args: any[]) => string) {}
+type AnyFunction = (...args: any[]) => any;
+type KeyGetter<Fn extends AnyFunction> = (...args: Parameters<Fn>) => string;
+function memoize<Fn extends AnyFunction>(fn: Fn, keyGetter?: KeyGetter<Fn>) {
+  const cache: Record<string, ReturnType<Fn>> = {};
+  return (...args: Parameters<Fn>) => {
+    const key = (keyGetter || JSON.stringify)(args);
+    if (!(key in cache)) {
+      cache[key] = fn(...args);
+    }
+    return cache[key];
+  };
+}
+
+function expensiveCalculation(a: number, b: number) {
+  const timeout = 10000;
+  const start = Date.now();
+  while (Date.now() <= start + timeout);
+  return a + b;
+}
+
+const memoizedExpensiveCalculation = memoize(expensiveCalculation);
+expensiveCalculation("not-a-number", 1);
+memoizedExpensiveCalculation("not-a-number", 1);
